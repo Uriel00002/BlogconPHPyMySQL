58d613166ce312fc85f7b0326631fce82ff4e57a
Uriel <uriellopezaquino1.0@gmail.com>
2021-07-14 20:18:01 -0500

    creacion de primeros documentos

---
+++ admin/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('RUTA', '')
+
+$bd_config = array {
+    'basededatos' => 'blog_practica' ,
+    'usuario' => 'root' ,
+    'pass' => ''
+    
+};
+
+?>
---
+++ views/index.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' reel='stylesheet' type='text.css'>
+    <link href='https://fonts.googleapis.com/css?family=Oswald' reel='stylesheet' type='text.css'>
+    <link reel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link reel="stylesheet" href="estilos.css">
+    <title>Blog</title>s
+</head>
+<body>
+     <header>
+         <div class="contenedor">
+              <div class="izquierda">
+                  <p><a href="#">Mi primer blog</a></p>
+              </div>
+              <div class="derecha">
+                  <form action="<?php echo"></form>
+              </div>
+         </div>
+     </header>
+</body>
+</html>
