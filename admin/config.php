c5debf5e01819c4a6cb0b4f5bd8dde2a1b2008c1
Uriel <uriellopezaquino1.0@gmail.com>
2021-07-15 11:55:45 -0500

    header, footer

--- admin/config.php
+++ admin/config.php
@@ -1,12 +1,21 @@
 <?php
 
-define('RUTA', '')
+define('RUTA' , 'http://localhost/blog/');
 
-$bd_config = array {
-    'basededatos' => 'blog_practica' ,
-    'usuario' => 'root' ,
+$bd_config = array( 
+    'basedatos' => 'blog_practica',
+    'usuario' => 'root',
     'pass' => ''
-    
-};
+);
+
+$blog_config = array( 
+    'post_por_pagina' => '2',
+    'carpeta_imagenes' => 'imagenes/',
+);
+
+$blog_admin = array( 
+    'usuario' => 'Uriel',
+    'password' => '123'
+);
 
 ?>
---
+++ views/footer.php
@@ -0,0 +1,5 @@
+<footer>
+      <p class="copyright">Copyright @2020 - 2021 Uriel Lopez Aquino</p>
+    </footer>
+</body>
+</html>
